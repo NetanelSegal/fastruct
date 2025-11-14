fca0f261e8439c68c0f42cc0a8455015cf54f1f7
Netanel <segal.netanel4@gmail.com>
2025-11-14 13:14:38 +0200

    home page design ready :)

--- src/sections/home/OurProcessSection.tsx
+++ src/sections/home/OurProcessSection.tsx
@@ -122,11 +122,12 @@ const OurProcessSection: React.FC<IProcess> = ({ title, steps }) => {
           ? Math.ceil(v)
           : Math.floor(v)
         : Math.round(v);
+    setStepNumber(v);
 
     animate(snappedIndex, rounded, {
-      ease: 'easeInOut',
+      ease: 'circInOut',
       delay: 0,
-      duration: 0.2,
+      duration: 0.1,
     });
   });
 
@@ -147,8 +148,6 @@ const OurProcessSection: React.FC<IProcess> = ({ title, steps }) => {
         y: p.y + stepsPositionYOffset,
       }))
     );
-
-    setStepNumber(fi);
   });
 
   useEffect(() => {
